dbf32aa76bf21bcbde3ba547727b917f7dc9d166
Maria Clara <mcfranca0107@gmail.com>
2022-05-02 15:10:41 -0300

    funcao getIdade

--- lib/model.dart
+++ lib/model.dart
@@ -6,24 +6,40 @@ class Pessoa {
   DateTime? Nascimento;
   int? Idade;
   Sexo? sexo;
-  double? Salario;
-  bool? Programador;
 
+  int getIdade(){
+    if(Nascimento == null){
+
+    }
+      final hoje = DateTime.now();
+      final idade = hoje.difference(Nascimento!);
+      return (idade.inDays ~/ 365);
+
+  }
   @override
   String toString(){
-    return "{nome=$Nome, cpf=$Cpf, nascimento=$Nascimento, idade=$Idade,"
-        "sexo=$sexo, salariario=$Salario, programador=$Programador}";
+    return "{nome=$Nome, cpf=$Cpf, nascimento=$Nascimento, idade=${getIdade()},"
+        "sexo=$sexo}";
   }
 }
 
+class Programador extends Pessoa{
+  double? Salario;
+  @override
+  String toString() {
+    return "{nome=$Nome, cpf=$Cpf, nascimento=$Nascimento, idade=${getIdade()},"
+        "sexo=$sexo, salariario=$Salario}";
+  }
+
+}
+
 void mainEntidades(){
-  Pessoa pessoa = new Pessoa();
+  Programador pessoa = Programador();
   pessoa.Nome = "Gabriel";
   pessoa.Nascimento = DateTime(2003,05,02);
   pessoa.Cpf = "100.645.109-99";
   pessoa.Idade = 19;
   pessoa.sexo = Sexo.masculino;
   pessoa.Salario = 2000.00;
-  pessoa.Programador = true;
   print(pessoa);
 }
